9de1ff1e43e34aeb7b0b86603c1340a4d24aa418
Jiyoon Bak <wldbs204@gmail.com>
2021-05-03 14:59:09 +0900

    Base code structure

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,20 @@
+/**
+ * @packageName     : ${package_name}
+ * @filewName       : ${file_name}
+ * @author          : Jiyoon back
+ * @date            : ${id:date('YYYY.MM.dd')}
+ * @description     :
+ * =======================================================
+ *      DATE         AUTHOR          NOTE
+ * -------------------------------------------------------
+ * ${id:date('YYYY.MM.dd')} JiYoon Bak 최초 생성
+ */
+package dao;
+
+/**
+ *
+ * @author mkaid
+ */
+public class UserDao {
+    
+}
