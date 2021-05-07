9a3e0ca4082e7445509983852a363cbb7b174c25
Jiyoon Bak <52561966+jiyoonB@users.noreply.github.com>
2021-05-07 22:03:32 +0900

    DB Connection 생성 및 MVC 패턴 구조화 (#6)
    * Edit pom.xml
    
    * DB 연결 Class 추가 및 MVC 패턴 구조 구현
    
    * 불필요한 파일 삭제

---
+++ src/main/java/dto/BookDto.java
@@ -0,0 +1,85 @@
+/**
+ * @packageName     : dto
+ * @filewName       : BookDto.java
+ * @author          : Jiyoon Bak
+ * @date            : 2021.05.07
+ * @description     : MVC Patterns에서 사용하는 Model
+ * =======================================================
+ *      DATE         AUTHOR          NOTE
+ * -------------------------------------------------------
+ * 2021.05.07        JiYoon Bak     최초 생성
+ */
+package dto;
+
+/**
+ * BookDto의 정보를 담고 있는 DTO class이다.
+ */
+public class BookDto {
+    private String id;
+    private String title;
+    private String genre;
+    private String author;
+    private String status;
+    private int count;
+    
+    public BookDto() {}
+    
+    // 모든 필드의 값을 생성자의 인자로 전달 받는 생성자 generate
+    public BookDto(String id, String title, String genre, String author, String status, int count){
+        super();
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.status = status;
+        this.count = count;
+    }
+    
+    public String getId(){
+        return id;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public String getGenre(){
+        return genre;
+    }
+    
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+    
+    public String getAuthor(){
+        return author;
+    }
+    
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    
+    public String getStatus(){
+        return status;
+    }
+    
+    public void setStatus(String status){
+        this.status = status;
+    }
+    
+    public int getCount(){
+        return count;
+    }
+    
+    public void setCount(int count){
+        this.count = count;
+    }
+}
