a11856053b7394bbdd8ceef97154fa12551be2ed
Jung-Han Park <wndgks6249@gmail.com>
2021-05-13 23:41:02 +0900

    로그인, 회원가입 구현 (#14)

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -8,6 +8,7 @@
  *      DATE         AUTHOR          NOTE
  * -------------------------------------------------------
  * 2021.05.01       JiYoon Bak      최초 생성
+ * 2021.05.13       Junghan Park   Login, Signup 메소드 추가
  */
 package controller;
 
@@ -23,12 +24,51 @@ public class UserController {
     private UserDao userdao;
     private TestView view;
     
-    public UserController(UserDto userdto, TestView view) {
+    public UserController(UserDto userdto, UserDao userdao, TestView view) {
         this.userdto = userdto;
         this.userdao = userdao;
     }
     
     public void updateView(){
         view.printUserDetails(userdto);
+    }
+    
+    public boolean Login(){
+        boolean result = false;
+        //입력받은 값과 데이터베이스의 값을 비교하기위한 for문
+        for(int i = 0; i < userdao.user.size(); i++){
+            if(userdto.getId().equals(userdao.user.get(i).getId()) && userdto.getPassword().equals(userdao.user.get(i).getPassword())){
+                result = true;
+                break;
+            }
+        }
+        
+        return result;
+    }
+    
+    public boolean Signup(){
+        boolean result = true;
+        int num = 1;
+        
+        if(!(userdao.user.isEmpty())){      //데이터베이스가 공백인지 확인하기 위한 조건문
+            for(int i = 0; i < userdao.user.size(); i++){   //입력받은 아이디가 이미 사용중인지 확인하기 위한 for문
+                if(userdto.getId().equals(userdao.user.get(i).getId())){
+                    result = false;
+                    System.out.println("This ID is already in use.");
+                    break;
+                }
+            }
+            if(result){     //입력받은 아이디가 사용가능한 아이디일 경우
+                num = userdao.user.size() + 1;
+                userdao.InputData(num, userdto.getId(), userdto.getPassword(), userdto.getName());
+                System.out.println("Singup success");
+            }
+        }
+        else{            //데이터베이스가 공백일 경우
+            userdao.InputData(num, userdto.getId(), userdto.getPassword(), userdto.getName());
+            System.out.println("Singup success");
+        }
+        
+        return result;
     }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -8,6 +8,7 @@
  *      DATE         AUTHOR          NOTE
  * -------------------------------------------------------
  * 2021.05.01       JiYoon Bak      최초 생성
+ * 2021.05.13       Junghan Park   SelectData, Inputdata메소드 추가 ArrayList 추가
  */
 package dao;
 
@@ -17,6 +18,7 @@ import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 /**
  * DB를 사용해 User Table의 데이터를 조회하거나 조작하는 기능을 구현한 클래스
@@ -24,8 +26,11 @@ import java.sql.PreparedStatement;
 public class UserDao {
     private static UserDao userDao;
     private final DBConnection db = DBConnection.getInstance();
-
-    public UserDao() {}
+    public ArrayList<UserDto> user = new ArrayList<>();     //데이터베이스에 들어있는 값들을 UserDto 객체형식으로 ArrayList에 저장하기 위한 ArrayList
+    
+    public UserDao() {
+        SelectData();       
+    }
     
     public static UserDao getInstance(){
         if(userDao == null){
@@ -73,5 +78,63 @@ public class UserDao {
         }
         
         return result;
+    }
+    
+    public void SelectData(){       //데이터베이스에 입력되어있는 값을 ArrayList형식으로 불러오는 메소드
+        Connection conn = db.connectDB();        
+        PreparedStatement pstm = null;
+        ResultSet rs = null;
+        
+        StringBuffer sql = new StringBuffer();
+        sql.append("SELECT * FROM USER");
+        
+        // 기능 생성시 아래와 같은 구조를 사용하여 close()를 해주어야 합니다.
+        try{
+            pstm = conn.prepareStatement(sql.toString());
+            rs = pstm.executeQuery();
+            
+            while(rs.next()){
+                user.add(new UserDto(rs.getInt("user_number"), rs.getString(2), rs.getString(3), rs.getString(4)));
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) try { rs.close(); } catch(Exception e) {}
+            if (pstm != null) try { pstm.close(); } catch(Exception e) {}
+            if (conn != null) try { conn.close(); } catch(Exception e) {}
+        }
+    }
+    
+    public void InputData(int user_number, String id, String password, String name){    //데이버베이스에 값을 입력하기 위한 메소드
+        int result = 0;
+        Connection conn = db.connectDB();
+        // SQL구문을 실행시키는 기능을 갖는 객체
+        PreparedStatement pstm = null;
+        // 쿼리를 실행한 결과를 받을 수 있다.
+        ResultSet rs = null;
+        
+        //SQL 문장 생성
+        StringBuffer sql = new StringBuffer();
+        sql.append("INSERT INTO USER (user_number, id, password, name) VALUES (?,?,?,?)");
+        
+        // 기능 생성시 아래와 같은 구조를 사용하여 close()를 해주어야 합니다.
+        try{
+            pstm = conn.prepareStatement(sql.toString());
+            
+            pstm.setInt(1, user_number);
+            pstm.setString(2, id);
+            pstm.setString(3, password);
+            pstm.setString(4, name);
+            
+            result = pstm.executeUpdate();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) try { rs.close(); } catch(Exception e) {}
+            if (pstm != null) try { pstm.close(); } catch(Exception e) {}
+            if (conn != null) try { conn.close(); } catch(Exception e) {}
+        }
     }
 }
