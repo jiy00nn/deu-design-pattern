228e4583cc30146f61576d65f496861a81a0d110
JongHoP <74240296+JongHoP@users.noreply.github.com>
2021-05-13 23:37:11 +0900

    Server Socket 통신 코드 (#13)

---
+++ src/main/java/db/Client.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import db.Send;
+  import java.io.DataOutputStream;
+import java.io.*;
+import java.net.Socket;
+
+/**
+ *
+ * @author ysj62
+ */
+public class Client {
+    public static void main(String[] arg)
+    {
+        Socket socket = null;            //Server와 통신하기 위한 Socket
+        DataInputStream in = null;        //Server로부터 데이터를 읽어들이기 위한 입력스트림
+        BufferedReader in2 = null;        //키보드로부터 읽어들이기 위한 입력스트림
+        DataOutputStream out = null;    
+        
+        try {
+
+            socket = new Socket("192.168.42.1",10002);    //서버로 접속
+            
+            in = new DataInputStream(socket.getInputStream());            
+            in2 = new BufferedReader(new InputStreamReader(System.in)); 
+            out = new DataOutputStream(socket.getOutputStream());        
+            System.out.print("이름을 입력하세요 : ");
+            String data = in2.readLine();   
+             out.writeUTF(data);       
+            //사용자가 채팅 내용을 입력 및 서버로 전송하기 위한 쓰레드 생성 및 시작
+            Thread th = new Thread(new Send(out));
+            th.start();
+        }catch(IOException e) {}
+        try {
+            //클라이언트의 메인 쓰레드는 서버로부터 데이터 읽어들이는 것만 반복.
+            while(true)
+            {
+                String str2 = in.readUTF();        
+                System.out.println(str2);
+            }
+        }catch(IOException e) {}
+    }
+}
