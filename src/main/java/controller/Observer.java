f3867b014df3cf71981cf874afb4065a216009e8
JongHoP <74240296+JongHoP@users.noreply.github.com>
2021-05-25 20:17:03 +0900

    observer

--- src/main/java/controller/Observer.java
+++ src/main/java/controller/Observer.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package controller;
-
+import java.util.UUID;
 public interface Observer {
-    public void update(int id, String title, String genre,String author,String status,int count); //주체에서 데이터의 변경사항을 전달 받기 위한 메서드
+    public void update(UUID id, String title, String genre,String author,String status,int count); //주체에서 데이터의 변경사항을 전달 받기 위한 메서드
 }
